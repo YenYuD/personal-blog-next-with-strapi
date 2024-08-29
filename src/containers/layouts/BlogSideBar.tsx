33db89522ebc1522ba607b089392af3e818aab43
YenYu <diaoemily0830@gmail.com>
2024-08-29 16:18:55 +0800

    refactor: mobile sidebar, abstract the nested accordian component

--- src/containers/layouts/BlogSideBar.tsx
+++ src/containers/layouts/BlogSideBar.tsx
@@ -7,7 +7,7 @@ import {
 	AccordionItem,
 	AccordionTrigger,
 } from '@/components/ui/accordion';
-import type { Language } from '@/service/type';
+import type { Language, SecondSubItemType } from '@/service/type';
 import { mapLanguageParam } from '@/service/utils/langaugeMapping';
 
 const SIDE_BAR_NAME = 'blog-sidebar';
@@ -17,6 +17,29 @@ function LinkItem({ label, href }: { label: string; href: string }) {
 		<Link className="first:font-semibold first:text-primary hover:underline leading-6" href={href}>
 			{label}
 		</Link>
+	);
+}
+
+function NestedAccordion({ subItems, lang }: { subItems: SecondSubItemType[]; lang: string }) {
+	return (
+		<>
+			{subItems.map(({ id, title, articles: { data } }) => (
+				<Accordion key={id} type="single" collapsible className="w-full pl-2">
+					<AccordionItem value={title} className="border-b-0">
+						<AccordionTrigger>
+							{title} ({data.length})
+						</AccordionTrigger>
+						<AccordionContent>
+							{data.map(({ id, attributes: { title } }) => (
+								<li key={id} className=" decoration-clone">
+									<LinkItem label={title} href={`/blog/${lang}/${id}`} />
+								</li>
+							))}
+						</AccordionContent>
+					</AccordionItem>
+				</Accordion>
+			))}
+		</>
 	);
 }
 
@@ -32,35 +55,29 @@ export default async function BlogSideBar({ lang }: { lang: Language }) {
 	const blogSidebar = sidebar.find((item) => item.attributes.title === SIDE_BAR_NAME)?.attributes
 		.sub;
 
-	if (!blogSidebar)
+	if (!blogSidebar) {
 		return (
 			<h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Oops! No data found.</h4>
 		);
+	}
 
 	return (
-		<nav
-			className="grid gap-4 text-sm text-muted-foreground max-h-[90vh] overflow-y-scroll"
-			x-chunk="dashboard-04-chunk-0"
-		>
-			{blogSidebar?.map(({ id, sub, title }) => (
-				<Accordion key={id} type="single" collapsible className="w-full">
-					<AccordionItem value={title}>
-						<AccordionTrigger className=" font-bold tracking-widest uppercase">
-							{title}
+		<>
+			<div className="lg:hidden gap-4 text-sm text-muted-foreground max-h-[90vh] overflow-y-scroll">
+				<Accordion type="single" collapsible>
+					<AccordionItem value="categories" className="border-0">
+						<AccordionTrigger className="font-bold tracking-widest text-muted-foreground uppercase no-underline hover:no-underline">
+							Categories
 						</AccordionTrigger>
-						<AccordionContent>
-							{sub.map(({ id, title, articles: { data } }) => (
-								<Accordion key={id} type="single" collapsible className="w-full pl-2">
-									<AccordionItem value={title} className="border-b-0">
-										<AccordionTrigger>
-											{title} ({data.length})
+						<AccordionContent className="pl-2">
+							{blogSidebar.map(({ id, sub, title }) => (
+								<Accordion key={id} type="single" collapsible className="w-full">
+									<AccordionItem value={title}>
+										<AccordionTrigger className=" font-bold tracking-widest uppercase">
+											{title}
 										</AccordionTrigger>
 										<AccordionContent>
-											{data.map(({ id, attributes: { title } }) => (
-												<li key={id}>
-													<LinkItem label={title} href={`/blog/${lang}/${id}`} />
-												</li>
-											))}
+											<NestedAccordion subItems={sub} lang={lang} />
 										</AccordionContent>
 									</AccordionItem>
 								</Accordion>
@@ -68,7 +85,24 @@ export default async function BlogSideBar({ lang }: { lang: Language }) {
 						</AccordionContent>
 					</AccordionItem>
 				</Accordion>
-			))}
-		</nav>
+			</div>
+			<div
+				className="hidden lg:grid gap-4 text-sm text-muted-foreground max-h-[90vh] overflow-y-scroll"
+				x-chunk="dashboard-04-chunk-0"
+			>
+				{blogSidebar.map(({ id, sub, title }) => (
+					<Accordion key={id} type="single" collapsible className="w-full">
+						<AccordionItem value={title}>
+							<AccordionTrigger className="font-bold tracking-widest uppercase">
+								{title}
+							</AccordionTrigger>
+							<AccordionContent>
+								<NestedAccordion subItems={sub} lang={lang} />
+							</AccordionContent>
+						</AccordionItem>
+					</Accordion>
+				))}
+			</div>
+		</>
 	);
 }
