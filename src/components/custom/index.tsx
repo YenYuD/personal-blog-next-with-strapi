a794ba1f65880f575fbbd993fcff3600fb8d02fc
YenYu <diaoemily0830@gmail.com>
2024-09-02 15:26:01 +0800

    feat: cld image component

--- src/components/custom/index.tsx
+++ src/components/custom/index.tsx
@@ -2,3 +2,4 @@ export { default as LoadingSkeleton } from './LoadingSkeleton';
 export { default as CardWrapper } from './CardWrapper';
 export { default as LinkItem } from './LinkItem';
 export { default as LanguageSwitcher } from './LanguageSwitcher';
+export { default as CldImage } from './CldImage';
