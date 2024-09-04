ff9b50c45ec58d96df5c96395653d1409f4d315a
YenYu <diaoemily0830@gmail.com>
2024-09-04 17:01:48 +0800

    fix: adjust the caching mechanism to set revalidation every hour.

--- src/service/server/fetcher.ts
+++ src/service/server/fetcher.ts
@@ -1,10 +1,13 @@
 export const fetcher = async (url: string, options?: RequestInit) => {
 	const res = await fetch(url, {
-		...options,
 		headers: {
 			'Content-Type': 'application/json',
 			authorization: `Bearer ${process.env.API_TOKEN}`,
 		},
+		next: {
+			revalidate: 1 * 60 * 60, // 1 hour
+		},
+		...options,
 	});
 	return await res.json();
 };
