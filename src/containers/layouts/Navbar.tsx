d06d9f5297b6289550d750cac2a5b45cccda4399
YenYu <diaoemily0830@gmail.com>
2024-09-04 16:59:16 +0800

    feat: add icons on navbars

--- src/containers/layouts/Navbar.tsx
+++ src/containers/layouts/Navbar.tsx
@@ -6,20 +6,32 @@ import { Button } from '@/components/ui/button';
 import { type LinkConfig, navbarConfig, siteTitle } from '@/constants/uiConfig';
 import { LinkItem, LanguageSwitcher } from '@/components/custom';
 import Provider from '@/Provider';
+import { GitHub, LinkedIn } from '@/components/custom/icons';
 
 type Props = {
 	config: LinkConfig;
 };
 
+const iconConfig = [
+	{
+		link: process.env.GIT_HUB_LINK,
+		icon: <GitHub className="h-5 w-5 opacity-90" />,
+	},
+	{
+		link: process.env.LINKEDIN_LINK,
+		icon: <LinkedIn className="h-6 w-6 opacity-90" />,
+	},
+];
+
 export default function Navbar({ config }: Props) {
 	return (
 		<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-10">
-			<div className="mx-auto grid w-full max-w-6xl items-center gap-12 md:grid-cols-[180px_1fr] lg:grid-cols-[150px_1fr]">
+			<div className="mx-auto grid w-full max-w-6xl items-center lg:gap-12 md:grid-cols-[180px_1fr] lg:grid-cols-[150px_1fr]">
 				<Link href="/" className="flex items-center gap-2 text-lg font-semibold">
 					<Image src="/favicon.ico" alt="logo" width={32} height={32} />
 					<span>{siteTitle}</span>
 				</Link>
-				<div className="max-w-6xl mx-auto flex flex-1 ml-[10px] items-center">
+				<div className="max-w-6xl mx-auto flex flex-1 ml-[10px] items-center w-full justify-between">
 					<nav className="hidden flex-col gap-6 text-lg font-bold md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
 						{navbarConfig.map((item) => (
 							<LinkItem key={item.href} label={item.label} href={item.href} />
@@ -28,6 +40,15 @@ export default function Navbar({ config }: Props) {
 							<LanguageSwitcher />
 						</Provider>
 					</nav>
+					<div className="flex">
+						{iconConfig.map((item) => (
+							<Button key={item.link} variant="ghost" size="icon">
+								<a target="_blank" href={item.link} key={item.link} rel="noreferrer">
+									{item.icon}
+								</a>
+							</Button>
+						))}
+					</div>
 				</div>
 			</div>
 			<Sheet>
@@ -48,6 +69,15 @@ export default function Navbar({ config }: Props) {
 						<Provider>
 							<LanguageSwitcher />
 						</Provider>
+						<div className="flex">
+							{iconConfig.map((item) => (
+								<Button key={item.link} variant="ghost" size="icon">
+									<a target="_blank" href={item.link} key={item.link} rel="noreferrer">
+										{item.icon}
+									</a>
+								</Button>
+							))}
+						</div>
 					</nav>
 				</SheetContent>
 			</Sheet>
