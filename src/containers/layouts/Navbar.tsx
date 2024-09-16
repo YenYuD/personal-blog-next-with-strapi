8356c58d1bf4ca835be9e04e978d79fe7a692499
YenYu <diaoemily0830@gmail.com>
2024-09-16 12:22:51 +0800

    refactor: navbar style adjustments

--- src/containers/layouts/Navbar.tsx
+++ src/containers/layouts/Navbar.tsx
@@ -23,16 +23,18 @@ const iconConfig = [
 	},
 ];
 
+export const NAB_BAR_HEIGHT = 64;
+
 export default function Navbar({ config }: Props) {
 	return (
-		<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-10">
+		<header className="fixed top-0 flex min-h-16 items-center gap-4 bg-transparent w-full px-4 md:px-6 max-h-16 z-[15]">
 			<div className="mx-auto grid w-full max-w-6xl items-center lg:gap-12 md:grid-cols-[180px_1fr] lg:grid-cols-[150px_1fr]">
-				<Link href="/" className="flex items-center gap-2 text-lg font-semibold">
-					<Image src="/favicon.ico" alt="logo" width={32} height={32} />
-					<span>{siteTitle}</span>
+				<Link href="/" className="flex items-center gap-2 text-lg ">
+					<Image src="/favicon.ico" alt="logo" width={36} height={36} />
+					<span className='text-[1.25rem]'>{siteTitle}</span>
 				</Link>
 				<div className="max-w-6xl mx-auto flex flex-1 ml-[10px] items-center w-full justify-between">
-					<nav className="hidden flex-col gap-6 text-lg font-bold md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+					<nav className="hidden flex-col gap-6 text-lg md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
 						{navbarConfig.map((item) => (
 							<LinkItem key={item.href} label={item.label} href={item.href} />
 						))}
@@ -53,8 +55,8 @@ export default function Navbar({ config }: Props) {
 			</div>
 			<Sheet>
 				<SheetTrigger asChild>
-					<Button variant="outline" size="icon" className="shrink-0 md:hidden">
-						<Menu className="h-5 w-5" />
+					<Button variant="outline" size="icon" className="shrink-0 md:hidden hover:text-foreground bg-transparent hover:bg-transparent border-none">
+						<Menu className="h-7 w-7 bg-transparent" />
 					</Button>
 				</SheetTrigger>
 				<SheetContent side="left">
