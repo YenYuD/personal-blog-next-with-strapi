fd2c245f1d1601c923b6765790ca6922f7a71fe8
YenYu <diaoemily0830@gmail.com>
2024-09-28 21:13:11 +0800

    feat: middleware for localization

--- next.config.mjs
+++ next.config.mjs
@@ -10,15 +10,6 @@ const nextConfig = {
 			},
 		],
 	},
-	redirects: async () => {
-		return [
-			{
-				source: '/',
-				destination: '/en',
-				permanent: true,
-			},
-		];
-	},
 	logging: {
 		fetches: {
 			fullUrl: true,
---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+import { type NextRequest, NextResponse } from 'next/server';
+
+const locales = ['en-US', 'zh-TW'];
+
+function getLocale(request: NextRequest) {
+	const negotiatorHeaders: Record<string, string> = {};
+	request.headers.forEach((value, key) => {
+		negotiatorHeaders[key] = value;
+	});
+	const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+	const defaultLocale = 'en-US';
+	const matchedLang = match(languages, locales, defaultLocale);
+	return matchedLang;
+}
+
+export function middleware(request: NextRequest) {
+	// Check if there is any supported locale in the pathname
+	const { pathname } = request.nextUrl;
+	const pathnameHasLocale = locales.some(
+		(locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+	);
+
+	if (pathnameHasLocale) return NextResponse.next();
+
+	const locale = getLocale(request);
+	request.nextUrl.pathname = `/${locale}${pathname}`;
+	return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+	matcher: [
+		// Skip all internal paths (_next)
+		'/((?!_next).*)',
+		'/',
+	],
+};
