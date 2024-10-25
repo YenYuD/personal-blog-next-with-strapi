b71daf838b50773c1c5431d8436aa60843c348ff
YenYu <diaoemily0830@gmail.com>
2024-10-25 16:51:49 +0800

    feat: add gradient background component and style adjustments

--- src/containers/layouts/Footer.tsx
+++ src/containers/layouts/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	const thisYear = new Date().getFullYear();
 	return (
-		<footer className="absolute bottom-[6px] text-sm lg:text-md left-[1rem] z-[15] tracking-wide cursor-default">
+		<footer className="text-sm lg:text-md left-[1rem] z-[15] tracking-wide cursor-default">
 			© {thisYear} Created by
 			<span className="hover:text-cyan-500 ml-1">
 				<a target="_blank" href={process.env.GIT_HUB_LINK} rel="noreferrer">
